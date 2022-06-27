fbbe79f3e6a9f4bcd565d861ae9680647514232e
akhrorbek <akhrornarzullayev.na@gmail.com>
2022-06-28 00:01:59 +0500

    init:Finished Bookmarked movies not local storage

--- js/script.js
+++ js/script.js
@@ -1,11 +1,93 @@
 const elResult = document.querySelector(".movie__result-num");
+const elMarkedResult = document.querySelector(".movie__result-number");
 const elMovieList = document.querySelector(".moviesBody__list");
 const elForm = document.querySelector(".form");
 const elSelect = document.querySelector(".form-select");
+const elBookmarkList = document.querySelector(".bookmarkList");
+
+const bookmark = [];
+
 
 elResult.textContent = movies.length;
 
 elSelect.innerHTML=null;
+
+
+
+/////BOOKMARKED KINOLARNI DELETE QILISH
+
+elBookmarkList.addEventListener("click", function (evt){
+    if(evt.target.matches(".delete-bookmark")){
+        elMarkedResult.textContent = bookmark.length - 1;
+        const bookmarkBtnIdDelete = evt.target.dataset.deleteBookmarkBtnId;
+        const foundBookmarkDeleteIndex = bookmark.findIndex(film=>film.id === bookmarkBtnIdDelete)
+
+        bookmark.splice(foundBookmarkDeleteIndex, 1)
+
+        elBookmarkList.innerHTML = null;
+
+        renderBookmarks(bookmark, elBookmarkList);
+    }
+
+})
+
+
+///////BOOKMARKED QISMIGA MARKED KINOLARNI CHIQARISH
+const renderBookmarks = function (arr){
+    arr.forEach((film) =>{
+
+        ///ADD ELEMENT
+        const bookmarkItem = document.createElement("li");
+        const bookmarkItemImg = document.createElement("img");
+        const bookmarkItemTitle = document.createElement("h5");
+        const bookmarkItemBtn = document.createElement("button");
+
+        /////SET ATTRIBUTE
+
+        bookmarkItem.setAttribute("class","bg-danger text-warning card mb-2 border-0");
+        bookmarkItem.style.width = "8rem";
+        bookmarkItemImg.classList.add("cardImage");
+        bookmarkItemImg.setAttribute("src", film.poster);
+        bookmarkItemTitle.classList.add("movieTitle","text-light");
+        bookmarkItemBtn.setAttribute("class", "delete-bookmark btn btn-danger text-warning border border-warning")
+
+        ///TEXT CONTENT
+
+        bookmarkItemTitle.textContent = film.title;
+        bookmarkItemBtn.textContent = "Remove"
+        bookmarkItemBtn.dataset.deleteBookmarkBtnId = film.id;
+
+        ////APPEND CHILD
+        elBookmarkList.appendChild(bookmarkItem);
+        bookmarkItem.appendChild(bookmarkItemImg);
+        bookmarkItem.appendChild(bookmarkItemTitle);
+        bookmarkItem.appendChild(bookmarkItemBtn);
+
+    })
+}
+renderBookmarks(bookmark, elBookmarkList);
+
+
+////BOOKMARK BUTTON BOSILGANDA MARKED QISMIGA QO'SHISH
+
+elMovieList.addEventListener("click", function (evt){
+   if(evt.target.matches(".bookmark-btn")){
+    elMarkedResult.textContent = bookmark.length + 1;
+    const selectBookmarkId = evt.target.dataset.bookmarkBtnId;
+    const foundBookmark = movies.find(film=> film.id === selectBookmarkId)
+
+        if(!bookmark.includes(foundBookmark))
+        bookmark.push(foundBookmark);
+
+        elBookmarkList.innerHTML = null;
+
+        renderBookmarks(bookmark, elBookmarkList);
+   };
+
+})
+
+
+//////KINO GANIRLARNI CHIQARISH
 
 const renderGenres = function (filmsArr){
 
@@ -36,7 +118,7 @@ const uniqueGenres = [];
 
 
 }
-
+////KINOLARNI CHIQARISH
 const renderMovies = function (moviesArr){
 
     moviesArr.forEach(function(film){
@@ -48,28 +130,32 @@ const renderMovies = function (moviesArr){
         const newDiv = document.createElement("div");
         const newTitle = document.createElement("h5");
         const newText = document.createElement("p");
-        const newBtn = document.createElement("button");
+        const bookmarkBtn = document.createElement("button");
         const newLink = document.createElement("a");
 
         ///SET ATTRIBUTE
 
-        newListItem.setAttribute("class", "card mb-5");
-        newListItem.style.width = "20rem";
+        newListItem.setAttribute("class", "card mb-5 border-0");
+        newListItem.style.width = "18rem";
         newImage.classList.add("cardImage");
         newImage.setAttribute("src", film.poster);
         newDiv.classList.add("card-body","p-3", "bg-danger", "text-warning");
         newTitle.classList.add("movieTitle","text-light");
         newText.classList.add("cardText");
-        newBtn.setAttribute("class", "btn btn-danger text-warning border border-warning");
+        bookmarkBtn.setAttribute("class", "bookmark-btn btn btn-danger text-warning border border-warning");
         newLink.setAttribute("class","btn btn-danger text-warning border border-warning mx-2");
 
         newTitle.textContent = film.title;
         newText.textContent = film.overview;
-        newBtn.textContent = "Bookmark";
+        bookmarkBtn.textContent = "Bookmark";
         newLink.textContent = "Watch Trailer";
 
-        ///SELECT GENRES
 
+        ///DATA SET
+
+        bookmarkBtn.dataset.bookmarkBtnId = film.id;
+        ///SELECT GENRES
+/*
         const genresList = document.createElement("ul");
 
         film.genres.forEach((genre) =>{
@@ -79,7 +165,7 @@ const renderMovies = function (moviesArr){
 
             genresList.appendChild(genresItem);
         })
-
+*/
         ///APPEND
 
         elMovieList.appendChild(newListItem);
@@ -87,9 +173,9 @@ const renderMovies = function (moviesArr){
         newListItem.appendChild(newDiv);
         newDiv.appendChild(newTitle);
         newDiv.appendChild(newText);
-        newDiv.appendChild(newBtn);
+        newDiv.appendChild(bookmarkBtn);
         newDiv.appendChild(newLink);
-        newDiv.appendChild(genresList);
+  //      newDiv.appendChild(genresList);
 
 
     })
@@ -98,13 +184,15 @@ renderGenres(movies, elMovieList);
 renderMovies(movies);
 
 
+
+////KINOLARNI SELECT ORQALI TANLAB CHIQARISH
 elForm.addEventListener("submit", function (evt){
     evt.preventDefault();
 
+    ///HTML dagi optionlarni o'chirish uchun
     elMovieList.innerHTML = null;
 
     const selectValue = elSelect.value;
-    console.log(selectValue);
 
 
     const selectedGenres = [];
@@ -113,6 +201,8 @@ elForm.addEventListener("submit", function (evt){
         if(selectValue === "All" || film.genres.includes(selectValue)){
             selectedGenres.push(film)
         }
+
+        elResult.textContent = selectedGenres.length;
     })
 
     renderMovies(selectedGenres, elMovieList);
